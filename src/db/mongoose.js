eefdc3a522df39ae073d1437d172671823eb9a62
Myron Mctyer <myronsdev@gmail.com>
2019-03-27 01:07:50 -0500

    Add additional fields to schema with basic validation and sanitation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -7,16 +8,18 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const Task = mongoose.model('Task', {
   description: {
-    type: String
+    type: String,
+    trim: true,
+    required: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   }
 })
 
 const task = new Task({
-  description: 'Clean house',
-  completed: false
+  description: '   Build desk'
 })
 
 task
@@ -30,16 +33,47 @@ task
 
 // const User = mongoose.model('User', {
 //   name: {
-//     type: String
+//     type: String,
+//     required: true,
+//     trim: true
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     minlength: 7,
+//     validate(pass) {
+//       if (pass.toLowerCase().includes('password')) {
+//         throw new Error('Password cannot contain "password"!')
+//       }
+//     }
 //   },
 //   age: {
-//     type: Number
+//     type: Number,
+//     default: 0,
+//     validate(value) {
+//       if (value < 0) {
+//         throw new Error('Age must be a positive number')
+//       }
+//     }
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     lowercase: true,
+//     validate(email) {
+//       if (!validator.isEmail(email)) {
+//         throw new Error('Email is invalid')
+//       }
+//     }
 //   }
 // })
 
 // const me = new User({
-//   name: 'Myron',
-//   age: 'mike'
+//   name: 'Basd',
+//   email: 'bad@ace.io',
+//   password: '   d2323   '
 // })
 
 // me.save()
