99f6ef770272ce8f16d8690b65216bed75d89900
Myron Mctyer <myronsdev@gmail.com>
2019-03-27 21:50:07 -0500

    Move models to own files and setup basic api for Create user and task

---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(pass) {
+      if (pass.toLowerCase().includes('password')) {
+        throw new Error('Password cannot contain "password"!')
+      }
+    }
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number')
+      }
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(email) {
+      if (!validator.isEmail(email)) {
+        throw new Error('Email is invalid')
+      }
+    }
+  }
+})
+
+module.exports = User
