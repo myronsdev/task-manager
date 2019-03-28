5573742ded9556a2610ac54e26f0d1f4bd34304d
Myron Mctyer <myronsdev@gmail.com>
2019-03-27 21:57:58 -0500

    add 201 responses for created resources

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.post('/users', (req, res) => {
   user
     .save()
     .then(() => {
-      res.send(user)
+      res.status(201).send(user)
     })
     .catch((err) => {
       res.status(400).send(err)
@@ -28,7 +28,7 @@ app.post('/tasks', (req, res) => {
   task
     .save()
     .then(() => {
-      res.send(task)
+      res.status(201).send(task)
     })
     .catch((err) => {
       res.status(400).send(err)
