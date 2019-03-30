6074558aec2e67e348a28acd0cac7c4f033853a7
Myron Mctyer <myronsdev@gmail.com>
2019-03-30 15:28:18 -0500

    Refactor models to use Schema, in order to add pre hooks for password hashing

--- src/index.js
+++ src/index.js
@@ -11,3 +11,19 @@ app.use(express.json(), userRouter, taskRouter)
 app.listen(port, () => {
   console.log(`Server is up on port: ${port}`)
 })
+
+const bcrypt = require('bcryptjs')
+
+const myFunc = async () => {
+  const password = 'Read12345!'
+
+  const hashedPassword = await bcrypt.hash(password, 8)
+
+  console.log(password)
+  console.log(hashedPassword)
+
+  const isMatch = await bcrypt.compare('Read12345!', hashedPassword)
+  console.log(isMatch)
+}
+
+myFunc()
