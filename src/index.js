3db58f986100b5bbcc50c17ef26e19a84fc620f5
Myron Mctyer <myronsdev@gmail.com>
2019-03-30 03:31:39 -0500

    Add patch and delete routes for users and tasks

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ app.patch('/users/:id', async (req, res) => {
   )
 
   if (!isValidOperation) {
-    return res.status(400).send({ error: 'Invalid updates!' })
+    return res.status(400).send({ error: 'Invalid update!' })
   }
 
   try {
@@ -66,6 +66,19 @@ app.patch('/users/:id', async (req, res) => {
     res.send(user)
   } catch (e) {
     res.status(400).send(e)
+  }
+})
+
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
+
+    if (!user) {
+      res.status(404).send()
+    }
+    res.send(user)
+  } catch (e) {
+    res.status(500).send()
   }
 })
 
@@ -92,6 +105,33 @@ app.get('/tasks/:id', async (req, res) => {
   }
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['description', 'completed']
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  )
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' })
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    })
+
+    if (!task) {
+      return res.status(404).send()
+    }
+
+    res.send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
 
@@ -103,6 +143,19 @@ app.post('/tasks', async (req, res) => {
   }
 })
 
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id)
+    if (!task) {
+      res.send(404).send()
+    }
+
+    res.send(task)
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
 app.listen(port, () => {
-  console.log(`Server is up on ${port}`)
+  console.log(`Server is up on port: ${port}`)
 })
