dc82083bfabc5a69e3eeae4742b7267e5a4f97fb
Myron Mctyer <myronsdev@gmail.com>
2019-03-29 22:51:48 -0500

    Add user update/patch route

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ app.post('/users', async (req, res) => {
 app.get('/users', async (req, res) => {
   try {
     const users = await User.find({})
+
     res.send(users)
   } catch (e) {
     res.status(500).send()
@@ -39,6 +40,32 @@ app.get('/users/:id', async (req, res) => {
     res.send(user)
   } catch (e) {
     res.status(500).send()
+  }
+})
+
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['name', 'email', 'password', 'age']
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  )
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' })
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    })
+    if (!user) {
+      return res.status(404).send()
+    }
+
+    res.send(user)
+  } catch (e) {
+    res.status(400).send(e)
   }
 })
 
