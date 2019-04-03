c684960283b975054ea9db68a535a4d08129f773
Myron Mctyer <myronsdev@gmail.com>
2019-04-02 23:42:58 -0500

    Add timestamps to models and explicitly write task schema -- allowing for schema option(timestamp)

--- src/models/user.js
+++ src/models/user.js
@@ -4,53 +4,58 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const Task = require('./task')
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(pass) {
-      if (pass.toLowerCase().includes('password')) {
-        throw new Error('Password cannot contain "password"!')
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 7,
+      validate(pass) {
+        if (pass.toLowerCase().includes('password')) {
+          throw new Error('Password cannot contain "password"!')
+        }
       }
-    }
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be a positive number')
+    },
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) {
+          throw new Error('Age must be a positive number')
+        }
       }
-    }
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(email) {
-      if (!validator.isEmail(email)) {
-        throw new Error('Email is invalid')
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(email) {
+        if (!validator.isEmail(email)) {
+          throw new Error('Email is invalid')
+        }
       }
-    }
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true
+        }
       }
-    }
-  ]
-})
+    ]
+  },
+  {
+    timestamps: true
+  }
+)
 
 userSchema.virtual('tasks', {
   ref: 'Task',
