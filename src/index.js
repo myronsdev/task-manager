d4833ebb8d7301ecf361643e315497579c1cb9c1
Myron Mctyer <myronsdev@gmail.com>
2019-04-04 21:59:31 -0500

    Create get avatar route and remove avatar property from toJSON method

--- src/index.js
+++ src/index.js
@@ -6,45 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//   res.status(503).send({ error: 'site is down. please try back again shortly' })
-// })
-
-// const multer = require('multer')
-// const upload = multer({
-//   dest: 'images',
-//   limits: {
-//     fileSize: 1000000
-//   },
-//   fileFilter(req, file, cb) {
-//     if (!file.originalname.match(/\.(doc|docx)$/)) {
-//       return cb(new Error('Please provide a word doc'))
-//     }
-
-//     cb(undefined, true)
-//     // cb(new Error('File must be a PDF'))
-//     // cb(undefined, true)
-//     // cb(undefined, false)
-//   }
-// })
-
-// const errorMiddleware = (req, res, next) => {
-//   throw new Error('from my middle ware')
-// }
-
-// app.post(
-//   '/upload',
-//   upload.single('upload'),
-//   (req, res) => {
-//     res.send()
-//   },
-//   (error, req, res, next) => {
-//     res.status(400).send({
-//       error: error.message
-//     })
-//   }
-// )
-
 app.use(express.json(), userRouter, taskRouter)
 
 app.listen(port, () => {
